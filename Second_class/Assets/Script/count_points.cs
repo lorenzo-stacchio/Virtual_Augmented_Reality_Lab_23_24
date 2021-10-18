7444eccf2b689775ceec07eaf274a4e44b6a12a0
Lorenzo Stacchio <lorenzo.stacchio.dev@gmail.com>
2021-10-18 10:13:20 +0200

    Count points

--- Second_class/Assets/Script/count_points.cs
+++ Second_class/Assets/Script/count_points.cs
@@ -5,25 +5,20 @@ using UnityEngine.UI;
 
 public class count_points : MonoBehaviour
 {
-    //declare a GameObject which will be used to spawn a prefab
-    [SerializeField]
-    private GameObject sphereToSpawn;
     private int points = 0;
-
+    private bool hit = false;
     private void OnTriggerEnter(Collider other) {
       Debug.Log("A pin touches the trigger");
-      points+=1;
+      this.points+=1;
    }
 
     void Update()
     {
-       // TODO: implementa cambio punteggio sulla scritta del testo del canvas, prendi componente testo e cambialo
-       //Debug.Log(transform.GetSiblingIndex());
-       //int my_index = transform.GetSiblingIndex();
-       //int child_of_parent = transform.parent.childCount; //it is two
-       //this.transform.parent.GetChild(0).GetChild(0).GetComponents();//<Text>().text = "Actual points: " + points;
-       //Debug.Log(child_of_parent);
-       //Debug.Log(transform.GetSiblingIndex());
-       //this.transform.parent.Find("Text").GetComponent<Text>().text = "Actual points: " + points;
+       Debug.Log("oooo");
+       // get first of my childs which is the canvas  
+       var father = this.transform.parent;
+       var canvas = father.transform.GetChild(0);
+       Text text_in_child_of_canvas = canvas.gameObject.GetComponentInChildren<Text>(); // in this case we want to access properties of the text object
+       text_in_child_of_canvas.text = "Points: " + this.points;
     }
 }
--- Second_class/README.md
+++ Second_class/README.md
@@ -4,3 +4,7 @@ https://github.com/ValveSoftware/steamvr_unity_plugin/releases/tag/2.2.0
 
 
 Fonte palla e birilli:
+
+
+# Troubleshooting
+- If controllers doesn't appear with the cited version of steamvrpluging, you have to delete the SteamVRInput and refresh the base bindings using Window -> SteamVRInput -> Save & generate
