315ee3623dfc88d65ed472988cd5b80db9149ecf
Lorenzo Stacchio <lorenzo.stacchio.dev@gmail.com>
2022-10-14 16:20:28 +0200

    mushroom collection implemented

---
+++ google_cardboard_sdk/Assets/Scripts/google_vr/MushroomController1.cs
@@ -0,0 +1,53 @@
+//-----------------------------------------------------------------------
+// <copyright file="ObjectController.cs" company="Google Inc.">
+// Copyright 2014 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Collections.Specialized;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>Controls interactable teleporting objects in the Demo scene.</summary>
+[RequireComponent(typeof(Collider))]
+public class MushroomController1 : MonoBehaviour
+{
+    /// <summary>Calls the Recenter event.</summary>
+    public void Recenter()
+    {
+        #if !UNITY_EDITOR
+                GvrCardboardHelpers.Recenter();
+        #else
+                    if (GvrEditorEmulator.Instance != null)
+                    {
+                        GvrEditorEmulator.Instance.Recenter();
+                    }
+        #endif  // !UNITY_EDITOR
+    }
+
+    /// <summary>Teleport this instance randomly when triggered by a pointer click.</summary>
+    /// <param name="eventData">The pointer click event which triggered this call.</param>
+    /// 
+    public void Collect(BaseEventData eventData)
+    {
+        WoodMan1.instance.collect_mushroom();
+        Destroy(gameObject);   
+    }
+
+
+
+}
+
---
+++ google_cardboard_sdk/Assets/Scripts/google_vr/WoodMan1.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class WoodMan1 : myPlayer
+{
+    public static WoodMan1 instance; // woodman is a singleton
+    private int mushrooms_collected;
+    private TextMeshProUGUI punteggio;
+
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        instance = this;
+        this.punteggio = GameObject.Find("Punteggio").GetComponent<TextMeshProUGUI>();
+        this.mushrooms_collected = 0;  
+    }
+
+
+    public void collect_mushroom(){
+        this.mushrooms_collected +=1;
+        this.punteggio.text = "Funghi raccolti: " + this.mushrooms_collected.ToString();
+    } 
+
+    public int collected_mushrooms(){
+        return this.mushrooms_collected;
+    }
+
+
+
+
+}
