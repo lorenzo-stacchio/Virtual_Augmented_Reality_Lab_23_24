d25885229ee45c14f368a656099063632d414f32
Lorenzo Stacchio <lorenzo.stacchio.dev@gmail.com>
2022-10-18 16:49:47 +0200

    load base htc example

---
+++ Htc_vive_basics/Assets/Scripts/Change_color.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+[RequireComponent(typeof(Interactable))]
+
+public class Change_color : MonoBehaviour
+{
+
+    private Vector3 oldPosition;
+    private Quaternion oldRotation;
+
+    // Standard SteamVR Interactable ri-definition variables
+    private Interactable interactable;
+    private Hand.AttachmentFlags attachmentFlags = Hand.defaultAttachmentFlags & (~Hand.AttachmentFlags.SnapOnAttach) & (~Hand.AttachmentFlags.DetachOthers) & (~Hand.AttachmentFlags.VelocityMovement);
+
+
+    //-------------------------------------------------
+    void Awake()
+    {
+        this.interactable = this.GetComponent<Interactable>();
+    }
+
+    //-------------------------------------------------
+    // Called every Update() while a Hand is hovering over this object
+    //-------------------------------------------------
+
+    private void HandHoverUpdate(Hand hand)
+    {
+        GrabTypes startingGrabType = hand.GetGrabStarting(); // --> Fired by the trigger!
+        bool isGrabEnding = hand.IsGrabEnding(this.gameObject);  // --> Check if the is not clicked anymore!
+
+        if (interactable.attachedToHand == null && startingGrabType != GrabTypes.None)
+        {
+            // Save our position/rotation so that we can restore it when we detach
+            oldPosition = transform.position;
+            oldRotation = transform.rotation;
+
+            // Call this to continue receiving HandHoverUpdate messages,
+            // and prevent the hand from hovering over anything else
+            hand.HoverLock(interactable);
+
+            // Attach this object to the hand
+            hand.AttachObject(gameObject, startingGrabType, attachmentFlags); // ---> Trigger on attached to hand!
+        }
+        else if (isGrabEnding)
+        {
+            // Detach this object from the hand
+            hand.DetachObject(gameObject);
+
+            // Call this to undo HoverLock
+            hand.HoverUnlock(interactable);
+
+            // Restore position/rotation
+            transform.position = oldPosition;
+            transform.rotation = oldRotation;
+        }
+
+    }
+
+    //-------------------------------------------------
+    // Called when this GameObject becomes attached to the hand
+    //-------------------------------------------------
+    private void OnAttachedToHand(Hand hand)
+    {
+        ChangeColor();
+    }
+
+
+    private void ChangeColor()
+    {
+        this.gameObject.GetComponent<MeshRenderer>().material.color = new Color(Random.Range(0f,1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+    }
+
+}
