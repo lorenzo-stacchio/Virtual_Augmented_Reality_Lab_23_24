7859c1d776395432540e1c0152ddb0283885d672
Lorenzo Stacchio <lorenzo.stacchio.dev@gmail.com>
2022-11-19 00:03:03 +0100

    new keyword detected in voice scene

---
+++ MRTK_basics/Assets/Scripts/Voice_Custom_Actions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Voice_Custom_Actions : MonoBehaviour
+{
+    public void debug()
+    {
+        Debug.Log("Vocal commands captured");
+    }
+
+}
