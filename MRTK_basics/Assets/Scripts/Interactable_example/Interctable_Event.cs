3d7fe12068ae715972f502297091863308fb15f6
Lorenzo Stacchio <lorenzo.stacchio.dev@gmail.com>
2022-11-19 16:57:59 +0100

    ux mrtk basics with interactable and custom events

---
+++ MRTK_basics/Assets/Scripts/Interactable_example/Interctable_Event.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using Microsoft.MixedReality.Toolkit.UI;
+
+public class Interctable_Event : MonoBehaviour
+{
+    Interactable interactable; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.interactable = this.gameObject.GetComponent<Interactable>();
+        this.interactable.OnClick.AddListener(Custom_touch);
+    }
+
+
+    // Update is called once per frame
+    void Custom_touch()
+    {
+        Debug.Log("Custom touch");
+        change_color();
+    }
+
+
+    void change_color()
+    {
+          this.gameObject.GetComponent<MeshRenderer>().material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+    }
+
+
+
+}
--- MRTK_basics/Assets/Scripts/Voice_Custom_Actions.cs
+++ MRTK_basics/Assets/Scripts/Voice_Custom_Actions.cs
@@ -11,6 +11,12 @@ public class Voice_Custom_Actions : MonoBehaviour
     }
 
 
+    public void debug_click()
+    {
+        Debug.Log("Click done");
+    }
+
+
     public void change_color(GameObject to_color)
     {
         to_color.gameObject.GetComponent<MeshRenderer>().material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
